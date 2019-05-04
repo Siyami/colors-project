a97d6112944be00f811b66b77457b0f288b4b58d
Siyami Avci <v-siavci@microsoft.com>
2019-05-03 23:16:25 -0700

    add pagination

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,36 @@ import { ColorsService } from './services/colors.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  colorsData: Array<string>;
+  colorsData: Array<object>;
+  displayArr: Array<object>;
+  totalPageNumbers: number;
+  pageArr = [];
+
   constructor(
     private _colorsService: ColorsService
   ) {}   
 
   ngOnInit() {
+   
     this.colorsData = this._colorsService.getColors();
+    this.totalPageNumbers = Math.ceil(this.colorsData.length / 12);
+    for (let i = 1; i < this.totalPageNumbers + 1; i++) {
+
+      this.pageArr.push(i);
+    }
+    debugger
+    this.changePage(1);
+
+    
+  }
+
+  changePage(number) {
+    this.displayArr = [];
+    let end = number * 12;
+    let start = end - 12;
+    for(let i = start; i < end; i++) {
+      if(!this.colorsData[i]) break;
+      this.displayArr.push(this.colorsData[i]);
+    }
   }
 }
