5fd42561ccba85540a32435ab4e0e4e161e94225
Siyami Avci <v-siavci@microsoft.com>
2019-05-03 18:23:50 -0700

    add colors service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ColorsService } from './services/colors.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'colors';
+  colorsData: Array<string>;
+  constructor(
+    private _colorsService: ColorsService
+  ) {}   
+
+  ngOnInit() {
+    this.colorsData = this._colorsService.getColors();
+  }
 }
